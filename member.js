15d10ad9c82a1ed692e24e9cb9ef56ccbc5972c0
kowama <k.kowama@gmail.com>
2023-12-11 21:05:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,27 @@
+function skillsMembers() {
+    var skills = [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Node",
+        "MongoDB"
+    ];
+
+    var members = [
+        "John",
+        "Jane",
+        "Jack",
+        "Jill",
+        "James",
+        "Jenny"
+    ];
+
+    for (var i = 0; i < members.length; i++) {
+        console.log(members[i]);
+    }
+
+    for (var i = 0; i < skills.length; i++) {
+        console.log(skills[i]);
+    }
+}
